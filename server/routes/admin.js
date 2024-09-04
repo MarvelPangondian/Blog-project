397f06d3ce0ac4f731338a023195193e57575389
Marvel Pangondian <marvel.pangondian01@gmail.com>
2024-09-04 23:04:57 +0700

    feat: login styling and page

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -6,4 +6,9 @@ router.route('/')
     res.render('./admin/index.ejs', {title:'Admin page', layout:"./admin/layouts/main-layout.ejs"});
   });
 
+router.route('/login')
+  .get((req,res) => {
+    res.render('./admin/login.ejs', {title:'login',layout:"./admin/layouts/main-layout.ejs"});
+  })
+
 module.exports = router;
